5b3d9e8efc47f69c178361cfe6042ce0b009df4d
HannahHunag <113820241+HannahHunag@users.noreply.github.com>
2023-01-04 00:12:52 +0800

    Update Line_Notify_assistant.js

--- Line_Notify_assistant.js
+++ Line_Notify_assistant.js
@@ -1,37 +1,52 @@
-function myFunction() {
-  // var token = "your_access_token";
+function phototo() { //圖發送
   // 取得要發送的文字內容，這裡是取得儲存格的資料發出
-  for(var time=1; time<=1; time++){
-    // 這裡要填入你的Google_Excel網址以及分頁名稱
-    if(time==1) var sheet = SpreadsheetApp.openById('Your_Google_Excel_URL').getSheetByName('Your_Google_Excel_分頁名稱');
-    // rownum 是抓取資料的欄位範圍
-    for(var rownum=130; rownum<=300; rownum++){
-      var notice = sheet.getRange(rownum,1,1,1).getValues();
-      if (notice=='Notice'){
-        // var titlecontent = sheet.getRange(1,2,1,6).getValues(); //只抓第1列B~G欄 共6個。 //sheet.getRange(列號,欄號,列範圍,欄範圍)
-        // var shortencontent = sheet.getRange(rownum,2,1,6).getValues(); //隨rownum變動列，抓B~G欄 共6個。 //sheet.getRange(列號,欄號,列範圍,欄範圍)
-        var content="";
-        // i 是設定抓取資料的列範圍
-        for(i= 3; i< 8; i++){
-          content = content + '\n' + sheet.getRange(1,i+2,1,1).getValues() + '：' + sheet.getRange(rownum,i+2,1,1).getValues();
-          // 請確認，將試算表日期整個欄位，調整成純文字格式。這樣才不會有類似GMT+0800 (香港標準時間)的顯示。
-        }
-        var message = "" + content;
-        // Line Notify 的 Access_Token
-        var token = "Your_Access_Token";
-        sendLineNotify(message, token);
+  // 這裡要放入你的Excel試算表的網址中間段，以及分頁名稱
+  var sheet = SpreadsheetApp.openById('你的Excel試算表的網址中間段').getSheetByName('Excel試算表分頁名稱');
+  // rownum 為抓取的列範圍
+  for(var rownum=2; rownum<=500; rownum++){
+    var today = sheet.getRange(rownum,1,1,1).getValues();
+    var imagesnum = sheet.getRange(rownum,5,1,1).getValues();
+    // 抓取 Excel 試算表中「Today」那一列
+    if (today=='Today' && imagesnum!=""){
+      //var titlecontent = sheet.getRange(1,2,1,6).getValues(); //只抓第1列B~G欄 共6個。 //sheet.getRange(列號,欄號,列範圍,欄範圍)
+      //var shortencontent = sheet.getRange(rownum,2,1,6).getValues(); //隨rownum變動列，抓B~G欄 共6個。 //sheet.getRange(列號,欄號,列範圍,欄範圍)
+      var content="";
+      // i 為抓取的欄位範圍
+      for(i= 3; i< 4; i++){
+        content ='\n' + sheet.getRange(rownum,4,1,1).getValues();
+        //請確認，將試算表日期整個欄位，調整成純文字格式。這樣才不會有類似GMT+0800 (香港標準時間)的顯示。
       }
-    }  
-  }
+      var message = "" + content + " ";
+      for(var imgurl=1; imgurl<=imagesnum; imgurl++){
+//下面var image的圖片訊息，必須使用外部連結，若是Google Drive裡面的圖片，必須要開啟權限分享然後製作成外部連結型式
+        var image = sheet.getRange(rownum,imgurl+5).getValue();
+        image = image + '#';
+        //var t = sheet.getRange(rownum,6,1,1).getValues();
+        //message = t;
+        if(imgurl==1){
+          var fullsizeimg = image;
+        }
+        if(imgurl==2){
+          var thumbnailimg = image;
+          // 這裡要放入你自己的 LINE_Notify_token
+          var token = "Your_LINE_Notify_token";
+          SendImagesNotify(message, fullsizeimg, thumbnailimg, token);
+        }
+        if(imgurl>1) message = '';
+      }  
+    }
+  }  
 }
-function sendLineNotify(message, token){//, image){
+function SendImagesNotify(message, fullsizeimg, thumbnailimg, token){ //錯誤說明參考 https://notify-bot.line.me/doc/en/
   var options =
    {
      "method"  : "post",
-     "payload" : {"message" : message},
-     //"payload" : {"message" : message, "imageThumbnail" : image, "imageFullsize" : image},  /*https://devdocs.line.me/files/sticker_list.pdf used syntax as in URL:https://www.oxxostudio.tw/articles/201806/line-notify.html*/
-     "headers" : {"Authorization" : "Bearer " + token}
+     //"payload" : {"message" : message}, /*https://devdocs.line.me/files/sticker_list.pdf used syntax as in URL:https://www.oxxostudio.tw/articles/201806/line-notify.html*/
+     "payload" : {"message" : message, "imageThumbnail" : thumbnailimg, "imageFullsize" : fullsizeimg}, /*https://devdocs.line.me/files/sticker_list.pdf used syntax as in URL:https://www.oxxostudio.tw/articles/201806/line-notify.html*/
+     "headers" : {"Authorization" : "Bearer " + token},
+     "muteHttpExceptions" : true
    };
    UrlFetchApp.fetch("https://notify-api.line.me/api/notify", options);
-
 }
+
+
